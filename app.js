b04ad5a5f16674dc5205a381b83394d7925c98e1
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-08 21:58:23 +0530

    Commented HTML code

--- app.js
+++ app.js
@@ -5,15 +5,18 @@
  * @modify date 2020-12-08 20:11:59
  * @desc [description]
  */
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// ##############################################
+// ############## Vanilla Javascript ############
+// ##############################################
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+// }
 
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal);
